2f7078cf21d3e22b66f40e1dfda5316064592cbe
Brian Richie Sr <brianrichiesr@gmail.com>
2023-11-19 12:00:18 -0800

    complete lab

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, json
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -52,18 +52,36 @@ def game_by_id(id):
 
     return response
 
-@app.route('/reviews')
+@app.route('/reviews', methods=['GET', 'POST'])
 def reviews():
 
-    reviews = []
-    for review in Review.query.all():
-        review_dict = review.to_dict()
-        reviews.append(review_dict)
+    if request.method == 'GET':
+        reviews = []
+        for review in Review.query.all():
+            review_dict = review.to_dict()
+            reviews.append(review_dict)
 
-    response = make_response(
-        reviews,
-        200
-    )
+        response = make_response(
+            reviews,
+            200
+        )
+    elif request.method == 'POST':
+        new_review = Review(
+            score=request.form.get("score"),
+            comment=request.form.get("comment"),
+            game_id=request.form.get("game_id"),
+            user_id=request.form.get("user_id"),
+        )
+
+        db.session.add(new_review)
+        db.session.commit()
+
+        review_dict = new_review.to_dict()
+
+        response = make_response(
+            review_dict,
+            201
+        )
 
     return response
 
@@ -81,6 +99,39 @@ def users():
     )
 
     return response
+
+@app.route('/reviews/<int:id>',
+           methods=['GET', 'DELETE', 'PATCH'])
+def review_by_id(id):
+    review = db.session.get(Review, id)
+    
+    if request.method == 'GET':
+        response_body = review.to_dict()
+        status = 200
+
+    elif request.method == 'PATCH':
+        for attr in request.form:
+            setattr(review, attr, request.form.get(attr))
+
+        db.session.add(review)
+        db.session.commit()
+
+        response_body = review.to_dict()
+        status = 200
+
+    elif request.method == 'DELETE':
+       db.session.delete(review)
+       db.session.commit()
+       response_body = {
+           "delete_successful": True,
+           "message": "Review deleted."
+       }
+       status = 200
+
+    return make_response(
+        response_body,
+        status
+    )
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
